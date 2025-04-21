2130399d1c660d555a22a4b86f96e403c48507b1
3 <153556337+eygwa@users.noreply.github.com>
2025-04-21 12:59:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -72,6 +72,101 @@ const getGroups = async () => {
     }
 };
 
+//WE ARE TESTING THIS RN
+//this is a little weird because i just copied it out of a typescript file, thats why (for now) there are some repeat functions. I will want to rewrite all this before deploying to clean up the express app
+const mkEmail = async (from, body) => {
+    // pnp/sp/emails doesnt actually work because it sends from an out of tdi email address & cant send to mailing lists :( if it did work it would look something like it does below
+    /*
+    if(!_sp) {console.error('sp undefined'); return;}
+    const emailProps : IEmailProperties = {
+        To:['it@tdi-bi.com'],
+        Subject:'ignore this',
+        Body:'this is a test email sent by parker from sharepoint & pnp/sp'
+    }
+    await _sp.utility.sendEmail(emailProps)
+    console.log('email sent! check inbox')
+    */
+    //graph solution for midware
+
+    const getAccessToken = async () => {
+        const params = new URLSearchParams();
+        params.append("grant_type", "client_credentials");
+        params.append("client_id", process.env.CLIENTID);
+        params.append("client_secret", process.env.CLIENTSECRET);
+        params.append("scope", "https://graph.microsoft.com/.default");
+        const response = await fetch(
+            `https://login.microsoftonline.com/${process.env.TENANTID}/oauth2/v2.0/token`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: params.toString(),
+            }
+        );
+
+        const data = await response.json();
+        if (!response.ok) {
+            throw new Error(
+                `Token request failed: ${JSON.stringify(data)}`
+            );
+        }
+
+        return data.access_token;
+    };
+
+    const sendEmail = async (
+        accessToken,
+        fromUserEmail,
+        toAddress
+    ) => {
+        const emailBody = {
+            message: {
+                subject: "Ignore this",
+                body: {
+                    contentType: "Text",
+                    content:
+                        body,
+                },
+                toRecipients: [
+                    {
+                        emailAddress: {
+                            address: toAddress,
+                        },
+                    },
+                ],
+            },
+            saveToSentItems: false,
+        };
+
+        const response = await fetch(
+            `https://graph.microsoft.com/v1.0/users/${from}/sendMail`,
+            {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(emailBody),
+            }
+        );
+
+        if (!response.ok) {
+            const error = await response.json();
+            throw new Error(`Send mail failed: ${JSON.stringify(error)}`);
+        }
+
+        console.log("Email sent successfully!");
+    };
+
+    try {
+        const token = await getAccessToken();
+        await sendEmail(token, "brooksmccall@tdi-bi.com", "parkerseeley@tdi-bi.com");
+    } catch (err) {
+        console.error("Error:", err);
+    }
+};
+ 
 app.get("/", (req, res) => {
     res.send(
         "haiii<br></br>to see email groups use ./group<br></br>to see all groups use ./groups"
@@ -174,6 +269,16 @@ app.get("/groupByName", async (req, res) => {
         group
     });
 });
+
+app.get("/testEmail", async (req,res)=>{
+    const body = req.query.body;
+    const from = req.body.from;
+    mkEmail('parkerseeley@tdi-bi.com', 'this is my new test body passed as a parameter');
+    mkEmail(from, body); // bweh
+    res.send(
+        "haiii<br></br>sending youre email..."
+    );
+})
 
 // we are gonna transition cache to 24 hours,
 // then we are going to create this as a route to clear cache and regenerate at like 12pm every day?
